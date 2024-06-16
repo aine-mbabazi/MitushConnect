6b847af541c1fcf4a4775c255db5f96aa3c0bc57
aine-mbabazi <patainembabazi@gmail.com>
2024-06-16 23:05:37 +0300

    Added landing page

---
+++ src/About/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+const About = ()=>{
+    return(
+                
+        <div>   
+          <h1 className='about'>About Us</h1>
+          
+          <div className='aboutdiv'>
+          <div className='aboutUs'>
+          <h1 className='aboutHeading'>MitushConnect</h1>
+          <p className="abouttext"> 
+            MitushConnect believes in creating and enabling sustainable partnerships
+            <br></br>
+            to between mitumba traders and 
+            recycling companies to reduce textile waste in Kenya.
+            <br></br> 
+            Our goal is to empower Mitumba traders
+            through impactful partnerships with recyclers.
+         </p>
+         </div>
+
+         <div className='image'>
+         <img src="/Images/about2.png"/>
+         </div>
+
+         </div>
+
+         </div>
+     
+    )
+}
+export default About;
