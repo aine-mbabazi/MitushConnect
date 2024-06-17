de5fb89297f3d76338ddab73011ee236984c239e
FaithHope <wachiraafaith>
2024-06-17 10:39:22 +0300

    Add Services

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import LandingPage from "./LandingPage"
 import About from "./About";
+import Services from "./Services";
 
 const App = () =>{
   return(
     <div>
       <LandingPage/>
       <About/>
+      <Services/>
     </div>
   )
 }
-import Team from "./Team";
-
-const   App = () =>{
-return(
-  <div>
-
-    <Team/>
-  </div>
-)
-}
+// import App from "./Team";
 export default App;
