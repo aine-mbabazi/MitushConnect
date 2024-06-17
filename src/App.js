413e6ad29db1c64bf7a86782b58d404b5ff9c380
Mutesi-alin <alinemutesiii@gmail.com>
2024-06-17 10:09:21 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import LandingPage from "./LandingPage"
 import About from "./About";
+import Footer from "./Footer";
 
 const App = () =>{
   return(
     <div>
       <LandingPage/>
       <About/>
+      <Footer/>
+    
+    {/* <div>
+       
     </div>
+      */}
+    </div>
+
   )
 }
-import Team from "./Team";
-
-const   App = () =>{
-return(
-  <div>
-
-    <Team/>
-  </div>
-)
-}
 export default App;
+// const   App = () =>{
+// return(
+//    <div>
+  
+//    <Team/>
+//     </div> )
+//    }
+//   export default App;
+  
+// import Team from "./Team";
+//  const   App = () =>{ return(
+//   <div>
+
+//    <Team/>
+//     </div>
+//     )}
+//     export default App;
